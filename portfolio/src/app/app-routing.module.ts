a2573cc12f54ec8cea6414a3bae6865f7c1c0ff9
Pablo ARRIEUMERLOU <p.arrieumerlou@groupe-aen.info>
2023-11-02 13:58:54 +0100

    Refactorisation et séparation des branches

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes } from '@angular/router';
-import { ProjectComponent } from './projets/projet/projet.component';
 
-const routes: Routes = [
-  { path: 'projects', component: ProjectComponent }
-];
 
 @NgModule({
   declarations: [],
--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -4,18 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
-import { ProjetComponent } from './projets/projet/projet.component';
-import { FormationComponent } from './formations/formation/formation.component';
-import { ProjetListComponent } from './projets/projet-list/projet-list.component';
-import { ProjetsComponent } from './projets/projets.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProjetComponent,
-    FormationComponent,
-    ProjetListComponent,
-    ProjetsComponent
   ],
   imports: [
     NgbModule,
