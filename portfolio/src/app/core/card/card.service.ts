a178af274a082d79881646f06cf6d72b383ec560
Pablo ARRIEUMERLOU <p.arrieumerlou@groupe-aen.info>
2023-11-03 17:14:02 +0100

    Ajout double URL deploy/test

--- portfolio/src/app/core/card/card.service.ts
+++ portfolio/src/app/core/card/card.service.ts
@@ -7,15 +7,16 @@ import { Card } from './card.model';
 export class CardService {
 
   private cardList: Card[]
+  deployDomain = "pabloarrieumerlou.com"
+  testDomain = "http://localhost:4200/"
+
 
   constructor() {
-    console.log(new URL("../assets/logos/Java.png", "http://localhost:4200/"));
-
     this.cardList = [
-      new Card("Angular", "Technologie", "Connaissance du framework web", new URL("../assets/logos/Angular.png", "http://localhost:4200/")),
-      new Card("PHP", "Technologie", "Développement en PHP", new URL("../assets/logos/PHP.png", "http://localhost:4200/")),
-      new Card("Java", "Technologie", "Différents environnements maîtrisés", new URL("../assets/logos/Java.png", "http://localhost:4200/")),
-      new Card("Javascript", "Technologie", "Différents environnements maîtrisés", new URL("../assets/logos/JS.png", "http://localhost:4200/"))
+      new Card("Angular", "Technologie", "Connaissance du framework web", new URL("/assets/logos/Angular.png", this.deployDomain)),
+      new Card("PHP", "Technologie", "Développement en PHP", new URL("/assets/logos/PHP.png", this.deployDomain)),
+      new Card("Java", "Technologie", "Différents environnements maîtrisés", new URL("/assets/logos/Java.png", this.deployDomain)),
+      new Card("Javascript", "Technologie", "Différents environnements maîtrisés", new URL("/assets/logos/JS.png", this.deployDomain))
     ]
   }
 
