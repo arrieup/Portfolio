2e31430e1f4b0f1670e5b0ce9dbb82f09943674a
Pablo ARRIEUMERLOU <p.arrieumerlou@groupe-aen.info>
2023-11-08 18:47:08 +0100

    Correction des imports

--- portfolio/src/app/core/card/card.component.ts
+++ portfolio/src/app/core/card/card.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input } from '@angular/core';
 import { Card } from './card.model';
-import { CardService } from './card.service';
 
 @Component({
   selector: 'app-card',
--- portfolio/src/app/services/project.service.ts
+++ portfolio/src/app/services/project.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Project } from '../interfaces/projet';
 
 @Injectable({
   providedIn: 'root'
