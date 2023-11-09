e7b619ffa1b6bc0ca695ce941b13a815cf3da1a5
Pablo ARRIEUMERLOU <p.arrieumerlou@groupe-aen.info>
2023-11-09 17:21:06 +0100

    Ajout page contact

---
+++ portfolio/src/app/contact/contact.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.sass']
+})
+export class ContactComponent {
+
+}
